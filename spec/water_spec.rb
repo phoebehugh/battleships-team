ca52553d37c1f5e6cb34afda7b89650dad9046cf
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-24 12:14:48 +0000

    Water can receive hits

--- spec/water_spec.rb
+++ spec/water_spec.rb
@@ -1,9 +1,13 @@
 require 'water'
 
 describe Water do
+  let(:water) { Water.new }
+
+  it "when created it is not hit" do
+    expect(water).not_to be_hit
+  end
 
   it "can receive a hit" do
-    water = Water.new
     water.receive_hit
     expect(water.hit?).to eq true
   end
